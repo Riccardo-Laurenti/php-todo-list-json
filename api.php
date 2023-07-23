4b7cee8bfc7a6a480ad4d8fd52ea32294235ddcf
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-07-23 14:28:18 +0200

    edit api

--- api.php
+++ api.php
@@ -5,3 +5,18 @@ include 'functions.php';
 header('Content-Type: application/json');
 
 $toDoListJson = file_get_contents('database.json');
+
+if (isset($_POST['action'])) {
+    $toDoList = json_decode($toDoListJson);
+
+    if ($_POST['action'] == 'sign') {
+        $toDoList = signTask($toDoList, $_POST['taskId']);
+    } else if ($_POST['action'] == 'delete') {
+        $toDoList = deleteTask($toDoList, $_POST['taskId']);
+    } else if ($_POST['action'] == 'add') {
+        $toDoList = addTask($toDoList, $_POST['taskText']);
+    }
+
+    $toDoListJson = json_encode($toDoList);
+    file_put_contents('database.json', $toDoListJson);
+}
